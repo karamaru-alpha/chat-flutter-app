cc1f083cca1a76486657930d1fbb009a6bd8d842
Karacky <mrnk3078@gmail.com>
2021-04-05 00:30:38 +0900

    feat: 起動時にサーバーからトークルームを取得

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,8 +1,12 @@
-import 'package:chat/state/room.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../proto/pb/room.pb.dart' as pb;
+import '../controller/room.dart';
+import '../service/room.dart';
+import '../state/room.dart';
+import '../model/room.dart';
 import '../provider/index.dart';
 import 'add_room.dart';
 
@@ -10,6 +14,21 @@ class HomeScreen extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final RoomState roomState = useProvider(roomProvider.state);
+    final RoomController roomController = useProvider(roomProvider);
+
+    useEffect(
+      () {
+        (() async {
+          pb.GetRoomsResponse res = await getRooms();
+          roomController.setRooms(
+            res.rooms.map((e) => Room(id: e.id, title: e.title)).toList(),
+          );
+        })();
+
+        return;
+      },
+      const <Object>[],
+    );
 
     return Scaffold(
       appBar: AppBar(
